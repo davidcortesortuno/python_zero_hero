41385fa109b8fde6c30501d289c6f1062d42f4cb
davidcortesortuno <david.cortes.o@gmail.com>
2024-11-29 16:05:57 -0300

    Organized code and printed results

--- arxiv_parser.py
+++ arxiv_parser.py
@@ -2,39 +2,6 @@ import urllib, urllib.request
 import bs4
 
 
-url = "http://export.arxiv.org/api/query?search_query=all:black+hole&start=0&max_results=10&sortBy=lastUpdatedDate"
-data = urllib.request.urlopen(url).read()  # Get a Bytes object with xml content
-
-# with open('arxivResult.xml', r) as F:
-#     fileText = F.read()
-
-bsData = bs4.BeautifulSoup(data, "xml")
-
-arxivEntries = bsData.find_all("entry")
-
-firstResult = arxivEntries[0]
-
-# print(firstResult)
-# author_list = []
-# for author in firstResult.find_all("author"):
-#     author_str = author.text.lstrip("\n").rstrip("\n")
-#     author_list.append(author_str)
-
-# author_list = [a.text for a in firstResult.find_all("name")]
-# title = firstResult.find("title").text
-# abstract = firstResult.find("summary").text
-#
-# data_dict = {}
-# data_dict["author_list"] = author_list
-# data_dict["title"] = title
-# data_dict["abstract"] = abstract
-
-# title_tag = "TITLE"
-# author_tag = "AUTHOR"
-
-# print("TITLE: {0}".format(data_dict["title"]))
-
-
 def format_bs_entry(bsentry: bs4.element.Tag) -> dict:
     """Format an entry of a BS result
 
@@ -49,15 +16,35 @@ def format_bs_entry(bsentry: bs4.element.Tag) -> dict:
 
     author_list = [name.text for name in bsentry.find_all("name")]
     title = bsentry.find("title").text
+    title = title.replace("\n", "")
     abstract = bsentry.find("summary").text
+    datep = bsentry.find("published").text
 
     data_dict = {}
     data_dict["author_list"] = author_list
     data_dict["title"] = title
     data_dict["abstract"] = abstract
+    data_dict["date"] = datep
 
     return data_dict
 
 
-res_dict = format_bs_entry(firstResult)
-print(res_dict)
+url = "http://export.arxiv.org/api/query?search_query=all:black+hole&start=0&max_results=10&sortBy=lastUpdatedDate"
+data = urllib.request.urlopen(url).read()  # Get a Bytes object with xml content
+
+# with open('arxivResult.xml', r) as F:
+#     fileText = F.read()
+
+bsData = bs4.BeautifulSoup(data, "xml")
+
+arxivEntries = bsData.find_all("entry")
+
+list_of_dictResults = []
+for result in arxivEntries:
+    dictRes = format_bs_entry(result)  # get dictionary with results
+    list_of_dictResults.append(dictRes)
+
+for d in list_of_dictResults:
+    print(d["date"])
+    print(d["title"])
+    print("--")
